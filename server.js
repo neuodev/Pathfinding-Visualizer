fc7da716af1bc58bef77149d483952df06c485b6
Ahmed Ibrahim <ib153507@gmail.com>
2021-03-26 19:07:46 +0200

    ADD server.js to server the build folder

--- server.js
+++ server.js
@@ -7,5 +7,6 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+const PORT = process.env.PORT || 5000;
 
-app.listen(9000);
+app.listen(PORT);
